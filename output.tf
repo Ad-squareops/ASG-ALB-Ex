e83be577b38e33f67387844f11c2577efbf91c4d
Ad-squareops <118443159+Ad-squareops@users.noreply.github.com>
2023-04-21 11:01:23 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -35,67 +35,67 @@ output "autoscaling_group_load_balancers" {
 
 output "lb_id" {
   description = "The ID and ARN of the load balancer we created."
-  value       = module.alb.lb_id
+  value       = module.asg.lb_id
 }
 
 output "lb_arn" {
   description = "The ID and ARN of the load balancer we created."
-  value       = module.alb.lb_arn
+  value       = module.asg.lb_arn
 }
 
 output "lb_dns_name" {
   description = "The DNS name of the load balancer."
-  value       = module.alb.lb_dns_name
+  value       = module.asg.lb_dns_name
 }
 
 output "lb_arn_suffix" {
   description = "ARN suffix of our load balancer - can be used with CloudWatch."
-  value       = module.alb.lb_arn_suffix
+  value       = module.asg.lb_arn_suffix
 }
 
 output "lb_zone_id" {
   description = "The zone_id of the load balancer to assist with creating DNS records."
-  value       = module.alb.lb_zone_id
+  value       = module.asg.lb_zone_id
 }
 
 output "http_tcp_listener_arns" {
   description = "The ARN of the TCP and HTTP load balancer listeners created."
-  value       = module.alb.http_tcp_listener_arns
+  value       = module.asg.http_tcp_listener_arns
 }
 
 output "http_tcp_listener_ids" {
   description = "The IDs of the TCP and HTTP load balancer listeners created."
-  value       = module.alb.http_tcp_listener_ids
+  value       = module.asg.http_tcp_listener_ids
 }
 
 output "https_listener_arns" {
   description = "The ARNs of the HTTPS load balancer listeners created."
-  value       = module.alb.https_listener_arns
+  value       = module.asg.https_listener_arns
 }
 
 output "https_listener_ids" {
   description = "The IDs of the load balancer listeners created."
-  value       = module.alb.https_listener_ids
+  value       = module.asg.https_listener_ids
 }
 
 output "target_group_arns" {
   description = "ARNs of the target groups. Useful for passing to your Auto Scaling group."
-  value       = module.alb.target_group_arns
+  value       = module.asg.target_group_arns
 }
 
 output "target_group_arn_suffixes" {
   description = "ARN suffixes of our target groups - can be used with CloudWatch."
-  value       = module.alb.target_group_arn_suffixes
+  value       = module.asg.target_group_arn_suffixes
 }
 
 output "target_group_names" {
   description = "Name of the target group. Useful for passing to your CodeDeploy Deployment Group."
-  value       = module.alb.target_group_names
+  value       = module.asg.target_group_names
 }
 
 output "target_group_attachments" {
   description = "ARNs of the target group attachment IDs."
-  value       = module.alb.target_group_attachments
+  value       = module.asg.target_group_attachments
 }
 
 
