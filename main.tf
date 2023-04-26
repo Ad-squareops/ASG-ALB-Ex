792caca244b7f66e1c5237c3b72b1b5f64fbead1
Ad-squareops <118443159+Ad-squareops@users.noreply.github.com>
2023-04-26 15:19:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,69 +1,40 @@
 locals {
-  vpc_cidr                  = "10.0.0.0/16"
-  Environment               = "prod"
-  Owner                     = "SquareOps"
-  app_name                  = "test"
-  region                    = "us-west-2"
-  vpc_id                    = "vpc-0b3f45c5755ae1d3e"
-  enabled_metrics           = ["GroupMinSize", "GroupMaxSize", "GroupDesiredCapacity", "GroupInServiceInstances", "GroupPendingInstances", "GroupStandbyInstances", "GroupTerminatingInstances", "GroupTotalInstances"]
-  zone_id                   = "Z025421120N1PKJMEL0SR"
-  domain_name               = "ldc.squareops.in"
-  host_headers              = "testadi.ldc.squareops.in"
-  hosted_zone_id            = "Z025421120N1PKJMEL0SR"
-  cert_enable               = true
+  vpc_cidr        = "10.0.0.0/16"
+  Environment     = "prod"
+  Owner           = "SquareOps"
+  app_name        = "demosops"
+  region          = "us-west-2"
+  vpc_id          = "vpc-0b3f45c5755ae1d3e"
+  zone_id         = "Z025421120N1PKJMEL0SR"
+  domain_name     = "ldc.squareops.in"
 }
 
 
 module "asg" {
-  source = "github.com/Ad-squareops/ASG-ALB-Module/module"
+  source = "/home/ubuntu/ASG-ALB-Module/module"
 
   Environment               = local.Environment
   app_name                  = local.app_name
   health_check_type         = "EC2"
-  vpc_zone_identifier       = ["subnet-0b1bf9f367133a41d", "subnet-0558682ffd7fa198e"]
-  enabled_metrics           = local.enabled_metrics
+  private_subnets           = ["subnet-0b1bf9f367133a41d", "subnet-0558682ffd7fa198e"]
   min_size                  = 1
   max_size                  = 1
   desired_capacity          = 1
-  wait_for_capacity_timeout = 0
-  default_instance_warmup   = 300
-
-
-  instance_refresh = {
-    strategy = "Rolling"
-    preferences = {
-      checkpoint_delay       = 600
-      checkpoint_percentages = [35, 70, 100]
-      instance_warmup        = 300
-      min_healthy_percentage = 50
-    }
-    triggers = ["tag"]
-  }
-
 
   # Launch template
-  launch_template_description = "Complete launch template example"
-  update_default_version      = true
   image_id                    = "ami-0db245b76e5c21ca1"
   instance_type               = "t3a.small"
-  ebs_optimized               = true
-  enable_monitoring           = true
 
-  
 
-#Load balancer
-  #name               = local.Environment
+  #Load balancer
   load_balancer_type = "application"
   vpc_id             = local.vpc_id
   public_subnets     = ["subnet-0ea38ad5d3b4d030b", "subnet-09d1456b4f93a6da4"]
   zone_id            = local.zone_id
   domain_name        = local.domain_name
-  host_headers       = local.host_headers
-  hosted_zone_id     = local.hosted_zone_id
 
   target_groups = [
     {
-      name             = local.Owner
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
@@ -75,7 +46,7 @@ module "asg" {
         unhealthy_threshold = 3
         timeout             = 5
         protocol            = "HTTP"
-        matcher             = "404"
+        matcher             = "200"
       }
     }
   ]
@@ -84,4 +55,9 @@ module "asg" {
     Owner       = local.Owner
     Environment = local.Environment
   }
+
+
+
+  asg_cpu_policy      = true
+  cpu_value_threshold = 70
 }
